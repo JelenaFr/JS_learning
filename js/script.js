ce19f6e55c50550f53252a38cdc5ae1bbecb2ffc
JelenaFr <jelena.freiberg@gmail.com>
2021-02-26 23:02:45 +0200

    Praktika 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-console.log(1);
-let number = 5;
-const leftBorderWidth = 1;  
-promt
+const numberOfFilms = +prompt("Skolko filmov vy posmoteli?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Odin iz poslednih posmotrennyh  filmov?", ""),
+    b = +prompt("Na skolko ocenite jego?", ""),
+    c = prompt("Odin iz poslednih posmotrennyh  filmov", ""),
+    d = +prompt("Na skolko ocenite jego?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
